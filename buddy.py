c5f64044f0334e59b0745bff76ff5ec85bf4f7be
Tyler Kang <tykang8402@outlook.com>
2020-01-14 10:42:16 -0800

    ini

---
+++ buddy.py
@@ -0,0 +1,8 @@
+# Income -> Curr Balance
+# Debt -> True Balance
+# Fixed Expenses -> Dollar Amount
+# Variable Expenses -> Percentage or Dollar Amount
+# Discretionary Expenses
+# ---------
+# Debt will be used to report true balance, but non-debt balance
+# will be reported as well
